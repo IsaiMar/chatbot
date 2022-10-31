70170a4d965e55c4fb13622fe4c9b09ddab29df6
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-10-31 12:45:03 -0600

    Created a chatbot named "Miguel"

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,34 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+
+my_bot = ChatBot(name='Miguel', read_only=True,
+                logic_adapters=
+['chatterbot.logic.MathematicalEvaluation',
+                                'chatterbot.logic.BestMatch'])
+
+list_trainer = ListTrainer(my_bot)
+small_talk = ['hi there',
+              'hi!',
+              'how do you do',
+              'how are you',
+              'i\'m fine',
+              'glad to hear that.',
+              'i feel awesome',
+              'excellent, glad to hear that.',
+              'not so good',
+              'sorry to hear that.',
+              'wha\'s your name',
+              'My name is Keeb, ask me a math question']
+
+math_talk_1 = ['pythagorean theorem',
+               'a squared plus b squared equals c squared']
+math_talk_2 = ['2+2',
+               '4 ask me a harder one.']
+math_talk_3 = ['law of cosines',
+               'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+            
+list_trainer = ListTrainer(my_bot)
+
+for item in (small_talk, math_talk_1, math_talk_2, math_talk_3):
+        list_trainer.train(item)
