161b28fe387b1933fca0081e3c466da22f166d59
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-11-01 20:28:19 -0600

    I made my first Chatbot

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-my_bot = ChatBot(name='Miguel', read_only=True,
+my_bot = ChatBot(name='Chatbot', read_only=True,
                 logic_adapters=
 ['chatterbot.logic.MathematicalEvaluation',
                                 'chatterbot.logic.BestMatch'])
@@ -19,7 +19,7 @@ small_talk = ['hi there',
               'not so good',
               'sorry to hear that.',
               'wha\'s your name',
-              'My name is Keeb, ask me a math question']
+              'My name is Chatbot, ask me a math question.']
 
 math_talk_1 = ['pythagorean theorem',
                'a squared plus b squared equals c squared']
@@ -32,3 +32,6 @@ list_trainer = ListTrainer(my_bot)
 
 for item in (small_talk, math_talk_1, math_talk_2, math_talk_3):
         list_trainer.train(item)
+
+corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+corpus_trainer.train('chatterbot.corpus.english')
