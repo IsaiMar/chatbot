37cb0535cc69f226e8e90975d60224d20ff6d6ae
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2022-11-09 10:37:30 -0700

    finished the readme

--- chatbot.py
+++ chatbot.py
@@ -16,7 +16,11 @@ small_talk = ['hi there',
               'not so good',
               'sorry to hear that.',
               'wha\'s your name',
-              'My name is Chatbot, ask me a math question.']
+              'My name is Chatbot, ask me a math question.',
+              "Hi, can I help you?",
+                "Sure, I'd like to book a flight to Iceland.",
+                "Your flight has been booked."]
+
 
 math_talk_1 = ['pythagorean theorem',
                'a squared plus b squared equals c squared']
@@ -29,6 +33,28 @@ list_trainer = ListTrainer(my_bot)
 
 for item in (small_talk, math_talk_1, math_talk_2, math_talk_3):
         list_trainer.train(item)
+
+name = "Bot Number 286" 
+monsoon = "rainy" 
+mood = "Smiley"
+resp = { 
+"what's your name?": [ 
+"They call me {0}".format(name), 
+"I usually go by {0}".format(name), 
+"My name is the {0}".format(name) ],
+"what's today's weather?": [ 
+"The weather is {0}".format(monsoon), 
+"It's {0} today".format(monsoon)], 
+"how are you?": [ 
+"I am feeling {0}".format(mood), 
+"{0}! How about you?".format(mood), 
+"I am {0}! How about yourself?".format(mood), ],
+"": [ 
+"Hey! Are you there?", 
+"What do you mean by these?", 
+ ],
+"default": [
+"This is a default message"] }
 
 corpus_trainer = ChatterBotCorpusTrainer(my_bot)
 corpus_trainer.train('chatterbot.corpus.english')
